1014ec1bfccea1d804915f1c9d317618d5b658e4
jaguzmano <jaguzmano@utn.edu.ec>
2024-07-16 15:42:45 -0500

    buscadores screen vendedor...

--- lib/widgets/customer/customer_widget.dart
+++ lib/widgets/customer/customer_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:mydealer/models/allCustomers.dart';
 import 'package:mydealer/models/customers.dart';
 import 'package:mydealer/views/google_map_page.dart';
 import 'package:mydealer/services/geocoding_service.dart';
+
 
 class CustomerWidget extends StatelessWidget {
   final Customer customer;
@@ -17,21 +17,30 @@ class CustomerWidget extends StatelessWidget {
       margin: const EdgeInsets.all(5.0),
       elevation: 5,
       child: ListTile(
-        title: Text('${customer.codCliente} - ${customer.nombreCliente}',
-            style: const TextStyle(fontWeight: FontWeight.bold)),
+        title: Text(
+          '${customer.codCliente} - ${customer.nombreCliente}',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
         subtitle: Padding(
           padding: const EdgeInsets.only(top: 8.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(customer.direccion),
+
               Text('Límite Crédito: ${customer.limiteCredito}'),
               Text('Saldo Disponible: ${customer.saldoPendiente}'),
             ],
           ),
         ),
-        leading: Icon(Icons.person_outline, color: Theme.of(context).primaryColor),
-        trailing: Icon(Icons.location_on_outlined, color: Theme.of(context).primaryColor),
+        leading: Icon(
+          Icons.person_outline,
+          color: Theme.of(context).primaryColor,
+        ),
+        trailing: Icon(
+          Icons.location_on_outlined,
+          color: Theme.of(context).primaryColor,
+        ),
         onTap: () async {
           LatLng? destination;
           if (customer.latitud != null && customer.longitud != null) {
@@ -60,86 +69,6 @@ class CustomerWidget extends StatelessWidget {
           }
         },
       ),
-    );
-  }
-}
-
-class AllCustomerWidget extends StatelessWidget {
-  final AllCustomers allCustomers;
-
-  const AllCustomerWidget({super.key, required this.allCustomers});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(5.0),
-      elevation: 5,
-      child: InkWell(
-        onTap: () {
-          showCustomerDetailsDialog(context, allCustomers);
-        },
-        child: ListTile(
-          title: Text('${allCustomers.codcliente} - ${allCustomers.nombre}',
-              style: const TextStyle(fontWeight: FontWeight.bold)),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(allCustomers.pais),
-                Text(allCustomers.provincia), //CAMBIAR SS
-                Text(allCustomers.ciudad),
-                Text('Límite Crédito: ${allCustomers.limitecredito}'),
-                Text('Saldo Disponible: ${allCustomers.saldopendiente}'),
-              ],
-            ),
-          ),
-          leading:
-              Icon(Icons.person, color: Theme.of(context).primaryColor),
-        ),
-      ),
-    );
-  }
-
-  void showCustomerDetailsDialog(
-      BuildContext context, AllCustomers allCustomers) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Detalles del Cliente"),
-          content: SingleChildScrollView(
-            child: ListTile(
-              title: Text('${allCustomers.codcliente} - ${allCustomers.nombre}',
-                  style: const TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Ruc: ${allCustomers.cedularuc}'),
-                    Text('País: ${allCustomers.pais}'),
-                    Text('Provincia: ${allCustomers.provincia}'),
-                    Text('Ciudad: ${allCustomers.ciudad}'),
-                    Text('Estad: ${allCustomers.estado}'),
-                    Text('Calificacion: ${allCustomers.calificacion}'),                 
-                    Text('Límite Crédito: ${allCustomers.limitecredito}'),
-                    Text('Saldo Disponible: ${allCustomers.saldopendiente}'),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cerrar'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }
